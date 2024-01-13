3bccb89791622b22c026971c72525afbbc5d447f
nslcoder <lokeshgurungwj@gmail.com>
2024-01-13 18:29:10 +0545

    chore: set up prettier, eslint, lint-staged & husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
+    'prettier',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
@@ -8,8 +9,55 @@ module.exports = {
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier', 'unused-imports', 'import-helpers'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    "prettier/prettier": 0,
+    "react/jsx-filename-extension": ["warn", { "extensions": [".js", ".jsx"] }],
+    "no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "no-console": "error",
+    "no-debugger": "error",
+    "arrow-body-style": ["error", "as-needed"],
+    "import-helpers/order-imports": [
+      "error",
+      {
+        "newlinesBetween": "always",
+        "groups": [
+          "/^react/",
+          "module",
+          "/^pages/",
+          "/^components/",
+          "/^assets/",
+          "/^constants/",
+          "/^context/",
+          "/^lib/",
+          "/^utils/",
+          ["parent", "sibling", "index"]
+        ],
+        "alphabetize": { "order": "asc", "ignoreCase": true }
+      }
+    ],
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        "vars": "all",
+        "varsIgnorePattern": "^_",
+        "args": "after-used",
+        "argsIgnorePattern": "^_"
+      }
+    ],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "padding-line-between-statements": [
+      "warn",
+      { "blankLine": "always", "prev": "*", "next": "return" },
+      { "blankLine": "always", "prev": ["const", "let", "var"], "next": "*" },
+      {
+        "blankLine": "any",
+        "prev": ["const", "let", "var"],
+        "next": ["const", "let", "var"]
+      }
+    ]
   },
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
     <>
-    <h1 className="">This is Achat List</h1>
+      <h1 className="">This is Achat List</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
